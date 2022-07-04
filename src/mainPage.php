512bd188cbb14258e07bc20f8c818069c9e54c9c
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-07-04 18:26:57 +0100

    latest backup, pages added

---
+++ src/mainPage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <title>Main Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href="../css/stylesheet.css">
+</head>
+
+</html>
+
+<?php
