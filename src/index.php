1530424dc2c13f6f480d022a661ca9bc44a331f5
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-07-04 12:11:34 +0100

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+    <title>Home Media</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href="../css/stylesheet.css">
+
+</head>
+<body>
+<h1 class="h1">Home Media</h1>
+</body>
+</html>
+<?php
