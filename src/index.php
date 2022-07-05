6d7a0b0ea3c0b53ed3a9403c16ce732faa0d70ac
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-07-05 17:02:39 +0100

    added login pages and styling

--- src/index.php
+++ src/index.php
@@ -8,7 +8,28 @@
 
 </head>
 <body>
-<h1 class="h1">Home Media</h1>
+<h1 class="h1">Home Media Collection</h1>
 </body>
 </html>
 <?php
+
+include "functions.php";
+?>
+<br>
+<div class="login">
+    <h1>Login Page</h1>
+    <form action="authenticate.php" method="post">
+
+        <label for="username">
+            <i class="fas fa-user"></i>
+
+            <input type="text" name="user" placeholder="Username" id="user" required>
+        </label>
+
+        <lable for="password">
+            <i class="fas fa-lock"></i>
+        </lable>
+        <input type="password" name="password" placeholder="Password" id="password" required>
+        <input type="submit" name="btnLogin" value="submit">
+    </form>
+</div>
